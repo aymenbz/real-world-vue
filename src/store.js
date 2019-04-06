0f67fc2fe58adc2401a8292f9466d2526f0ad170
Aymen Bouzoumita <aymenweb5@gmail.com>
2019-04-06 10:42:13 +0100

    different mapState

--- src/store.js
+++ src/store.js
@@ -5,7 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: { id: "abcs12", name: "Aymen" }
+    user: { id: "abcs12", name: "Aymen" },
+    categories: [
+      { id: "azr3", name: "zefuih" },
+      { id: "azrr3", name: "sdgggggg" },
+      { id: "azr433", name: "gsdsdg" }
+    ]
   },
   mutations: {},
   actions: {}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <h1>Create An Event {{userName}}</h1>
-    <p>This Event was created By {{userName}}</p>
+    <h1>Create An Event {{user.name}}</h1>
+    <p>This Event was created By {{user.id}}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat.id">{{cat.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import { mapState } from "vuex";
+import { mapState } from "vuex";
 
-  export default {
-    computed: mapState({
-      userName: state => state.user.name,
-      userID: state => state.user.id,
-      categories: state => state.categories
-    })
-  };
+export default {
+  computed: mapState(["user", "categories"])
+};
 </script>
 
 <style>
