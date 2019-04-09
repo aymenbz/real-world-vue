4a629125ed1ea3b24c65fecaf10d42530d50a5b9
Aymenbz <aymenweb5@gmail.com>
2019-04-09 20:11:36 +0100

    fetch events

--- src/store.js
+++ src/store.js
@@ -29,6 +29,9 @@ export default new Vuex.Store({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
     }
   },
   actions: {
@@ -36,6 +39,15 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then(response => {
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   },
   getters: {
